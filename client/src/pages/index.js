ae02ff50ffe3e999d05a67e8178ba0f46c785138
Ankan112 <ankanhalder112@gmail.com>
2023-08-24 00:36:35 +0600

    project setup done

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-4xl text-center bg-red-400">Home</h1>
+      <h1 className="text-4xl font-bold text-center bg-red-400">Home</h1>
     </div>
   );
 };
