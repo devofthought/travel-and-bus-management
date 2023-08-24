34ba7646f4f504801fe9ac555b7a248a5b75996e
Abdullah-Sakib <abdullahsakib888@gmail.com>
2023-08-24 20:45:15 +0600

    feat: Added feedback modal

---
+++ server/src/app/modules/feedback/feedback.router.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import { FeedbackValidation } from './feedback.validation'
+import validateRequest from '../../middlewares/validateRequest'
+import { FeedbackController } from './feedback.controller'
+const router = express.Router()
+
+router.get('/', FeedbackController.getAllFeedback)
+router.post(
+  '/',
+  validateRequest(FeedbackValidation.createFeedbackZodSchema),
+  FeedbackController.createFeedback
+)
+router.patch(
+  '/:id',
+  validateRequest(FeedbackValidation.updateFeedbackZodSchema),
+  FeedbackController.updateFeedback
+)
+router.get('/:user_id', FeedbackController.getSingleUserFeedback)
+router.delete('/:id', FeedbackController.deleteFeedback)
+
+export const FeedbackRoutes = router
