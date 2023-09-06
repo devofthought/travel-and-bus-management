cf7148070963c45c0d7f084629482def0b15a0b6
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-09-06 20:53:53 +0600

    [update] admin trip create

--- client/src/containers/AdminDashboard/Trip/CreateTrip/BusTable.js
+++ client/src/containers/AdminDashboard/Trip/CreateTrip/BusTable.js
@@ -1,14 +1,17 @@
-import { Avatar, Rate, Table, Typography } from "antd";
-import { useState } from "react";
+import { useGetAllBusQuery } from "@/redux/bus/busApi";
+import { Avatar, Table, Typography } from "antd";
 
 const BusTable = () => {
-  const [dataSource, setDataSource] = useState([]);
+  const { data } = useGetAllBusQuery();
 
   const columns = [
     {
       title: "Sr.",
       dataIndex: "sr",
       minWidth: 200,
+      render: (text, record, index) => {
+        return `${index + 1}`;
+      },
     },
     {
       title: "Bus Code",
@@ -16,10 +19,28 @@ const BusTable = () => {
       minWidth: 200,
     },
     {
-      title: "Availability Status",
+      title: "Current status",
       dataIndex: "availability_status",
       minWidth: 200,
-      sorter: (a, b) => a.availability_status - b.availability_status,
+      render: (availability_status) => {
+        return (
+          <p
+            className={
+              availability_status === "servicing"
+                ? "bg-[rgba(255,189,90,.2)] text-[#ffc107] rounded pl-2"
+                : availability_status === "standBy"
+                ? "bg-[rgba(28,213,174,.2)] text-[#38cab3] rounded pl-2"
+                : availability_status === "transit"
+                ? "bg-[#7CB9E8] text-[#0039a6] rounded pl-2"
+                : availability_status === "rest"
+                ? "bg-[#A3C1AD] text-[#002244] rounded pl-2"
+                : "bg-[rgba(247,79,117,.2)] text-[#f74f75] rounded pl-2"
+            }
+          >
+            {availability_status}
+          </p>
+        );
+      },
     },
     {
       title: "Brand Name",
@@ -32,9 +53,12 @@ const BusTable = () => {
       minWidth: 200,
     },
     {
-      title: "Bus Image",
+      title: "Image",
       dataIndex: "bus_image",
       minWidth: 200,
+      render: (bus_image) => {
+        return <Avatar size={16} src={bus_image?.avatar} />;
+      },
     },
   ];
   return (
@@ -42,7 +66,7 @@ const BusTable = () => {
       <Typography.Title level={4}>Bus</Typography.Title>
       <Table
         columns={columns}
-        dataSource={dataSource}
+        dataSource={data?.data}
         pagination={{
           pageSize: 5,
         }}
