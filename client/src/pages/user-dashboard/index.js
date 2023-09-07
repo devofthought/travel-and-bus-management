f862404bc292685e3d427c4921428475c5f37d20
Abdullah Sakib <abdullahsakib888@gmail.com>
2023-09-07 21:56:15 +0600

    merged with dev

--- client/src/pages/user-dashboard/index.js
+++ client/src/pages/user-dashboard/index.js
@@ -1,7 +1,5 @@
 import UserDashboardLayout from "@/layouts/UserDashboardLayout";
-import { openUserDashboard } from "@/redux/user/userSlice";
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
 
 const UserDashboard = () => {
   return (
