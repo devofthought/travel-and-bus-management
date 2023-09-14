2629c63a0551be89efdd47d4c4c2629792740a73
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-09-14 18:54:19 +0600

    [update]: some design change

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -9,6 +9,7 @@ import Technologies from "@/containers/HomePage/Technologies";
 import React from "react";
 import RootLayout from "@/layouts/RootLayout";
 import OurExclusiveOffer from "@/containers/HomePage/OurExclusiveOffer";
+import ExclusiveOffer from "@/containers/HomePage/ExclusiveOffer";
 
 const HomePage = () => {
   return (
@@ -17,7 +18,8 @@ const HomePage = () => {
       <Technologies />
       <WhatWeOffer />
       <ReserveABus />
-      <OurExclusiveOffer />
+      {/* <OurExclusiveOffer /> */}
+      <ExclusiveOffer />
       <Review />
       <FAQ />
       <OurBuses />
