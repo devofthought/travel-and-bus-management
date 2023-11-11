5641ac701a059fca60ee0ce187581ad7344d2a06
Abdullah Sakib <abdullahsakib888@gmail.com>
2023-11-11 11:54:07 +0600

    feat: Login and Register API intergration

--- client/src/containers/LoginPage/LoginForm.jsx
+++ client/src/containers/LoginPage/LoginForm.jsx
@@ -1,57 +1,81 @@
-import { Form, Input } from "antd";
+import { Form, Input, message } from "antd";
 import Button from "@/components/UI/Button";
+import { useLoginMutation } from "@/redux/user/userApi";
+import { useEffect } from "react";
+
+
 const LoginForm = () => {
+  const [login, { isSuccess }] = useLoginMutation();
+  const [form] = Form.useForm(); // Create a form instance
+  const [messageApi, contextHolder] = message.useMessage();
+
   const onFinish = (values) => {
-    console.log("Success:", values);
+    login(values);
+
+    // Reset the form fields after successful submission
+    form.resetFields();
   };
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
+
+  useEffect(() => {
+    if (isSuccess) {
+      messageApi.open({
+        type: "success",
+        content: "Login successfull",
+      });
+    }
+  }, [isSuccess]);
   return (
-    <Form
-      className=""
-      name="basic"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <label className=" font-medium">Email Address</label>
-      <Form.Item
-        className="w-full h-8 mb-4"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: "Please input your valid email address!",
-          },
-        ]}
+    <>
+      {contextHolder}
+      <Form
+        form={form}
+        className=""
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input className="h-8 mt-1" />
-      </Form.Item>
+        <label className=" font-medium">Email Address</label>
+        <Form.Item
+          className="w-full h-8 mb-4"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your valid email address!",
+            },
+          ]}
+        >
+          <Input className="h-8 mt-1" />
+        </Form.Item>
 
-      <label className=" font-medium">Password</label>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          },
-        ]}
-      >
-        <Input.Password className="h-8 mt-1" />
-      </Form.Item>
-      <p className="text-right text-sm text-black cursor-pointer -mt-5 w-full mb-5 hover:underline">
-        Forget your password
-      </p>
+        <label className=" font-medium">Password</label>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password className="h-8 mt-1" />
+        </Form.Item>
+        <p className="text-right text-sm text-black cursor-pointer -mt-5 w-full mb-5 hover:underline">
+          Forget your password
+        </p>
 
-      <Form.Item>
-        <Button btnName="Log in" styles="w-full py-2"></Button>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button btnName="Log in" styles="w-full py-2"></Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
--- client/src/containers/SignupPage/SignupForm.jsx
+++ client/src/containers/SignupPage/SignupForm.jsx
@@ -1,90 +1,120 @@
-import { Form, Input } from "antd";
+import { Form, Input, message } from "antd";
 import Button from "@/components/UI/Button";
+import { useSignUpMutation } from "@/redux/user/userApi";
+import { useEffect } from "react";
 
 const SignUpForm = () => {
+  const [signUp, { isSuccess }] = useSignUpMutation();
+  const [form] = Form.useForm(); // Create a form instance
+  const [messageApi, contextHolder] = message.useMessage();
+
   const onFinish = (values) => {
-    console.log("Success:", values);
+    // console.log("Success:", values);
+    const modified = {
+      name: values.firstName + " " + values.lastName,
+      ...values,
+    };
+    delete modified.firstName;
+    delete modified.lastName;
+    signUp(modified);
+
+    // Reset the form fields after successful submission
+    form.resetFields();
   };
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
+
+  useEffect(() => {
+    if (isSuccess) {
+      messageApi.open({
+        type: "success",
+        content: "Registration successfull",
+      });
+    }
+  }, [isSuccess]);
   return (
-    <Form
-      name="basic"
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <label className=" font-medium">First Name</label>
-      <Form.Item
-        className="w-full h-8 mb-4"
-        name="firstName"
-        rules={[
-          {
-            required: true,
-            message: "Please input your First Name!",
-          },
-        ]}
-      >
-        <Input className="h-8 mt-1" />
-      </Form.Item>
-      <label className=" font-medium">Last Name</label>
-      <Form.Item
-        className="w-full h-8 mb-4"
-        name="lastName"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Last Name!",
-          },
-        ]}
-      >
-        <Input className="h-8 mt-1" />
-      </Form.Item>
-      <label className=" font-medium">Email Address</label>
-      <Form.Item
-        className="w-full h-8 mb-4"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: "Please input your valid email address!",
-          },
-        ]}
-      >
-        <Input className="h-8 mt-1" />
-      </Form.Item>
-      <label className=" font-medium">Password</label>
-      <Form.Item
-        className="w-full h-8 mb-4"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Password!",
-          },
-        ]}
-      >
-        <Input.Password className="h-8 mt-1" />
-      </Form.Item>
+    <>
+      {contextHolder}
 
-      <label className=" font-medium">Confirm Password</label>
-      <Form.Item
-        name="confirmPassword"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Confirm Password!",
-          },
-        ]}
+      <Form
+        form={form}
+        name="basic"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input.Password className="h-8 mt-1" />
-      </Form.Item>
+        <label className=" font-medium">First Name</label>
+        <Form.Item
+          className="w-full h-8 mb-4"
+          name="firstName"
+          rules={[
+            {
+              required: true,
+              message: "Please input your First Name!",
+            },
+          ]}
+        >
+          <Input className="h-8 mt-1" />
+        </Form.Item>
+        <label className=" font-medium">Last Name</label>
+        <Form.Item
+          className="w-full h-8 mb-4"
+          name="lastName"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Last Name!",
+            },
+          ]}
+        >
+          <Input className="h-8 mt-1" />
+        </Form.Item>
+        <label className=" font-medium">Email Address</label>
+        <Form.Item
+          className="w-full h-8 mb-4"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your valid email address!",
+            },
+          ]}
+        >
+          <Input className="h-8 mt-1" />
+        </Form.Item>
+        <label className=" font-medium">Password</label>
+        <Form.Item
+          className="w-full h-8 mb-4"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input.Password className="h-8 mt-1" />
+        </Form.Item>
 
-      <Form.Item>
-        <Button btnName="Sign up" styles="w-full py-2"></Button>
-      </Form.Item>
-    </Form>
+        <label className=" font-medium">Confirm Password</label>
+        <Form.Item
+          name="confirm_password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Confirm Password!",
+            },
+          ]}
+        >
+          <Input.Password className="h-8 mt-1" />
+        </Form.Item>
+
+        <Form.Item>
+          <Button btnName="Sign up" styles="w-full py-2"></Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
