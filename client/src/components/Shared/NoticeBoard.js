b9e726abc836347a9011f3fae68eaf3a300a1de1
Md Kamruzzaman <kamruzzaman-dev@users.noreply.github.com>
2023-11-17 10:39:28 +0600

    Update NoticeBoard.js

--- client/src/components/Shared/NoticeBoard.js
+++ client/src/components/Shared/NoticeBoard.js
@@ -4,8 +4,8 @@ const NoticeBoard = () => {
     <div className="bg-[#1b4965]">
       <Marquee speed={80}>
         <p className="text-xl text-white">
-          Here, on bug/issue in the backend. currently backend is under
-          maintenance. Currently this website is run on the local fake data.
+          Here, on bug/issue in the backend. Currently backend is under
+          maintenance and this website is run on the local fake data. So, dynamically function is now available now.
         </p>
       </Marquee>
     </div>
