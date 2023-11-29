0e48a44e10d98527bcbdb0cb34c9738f74f400a8
Md Kamruzzaman <kamruzzaman-dev@users.noreply.github.com>
2023-11-29 15:24:23 +0600

    booking code

--- server/src/app/modules/booking/booking.interface.ts
+++ server/src/app/modules/booking/booking.interface.ts
@@ -1,19 +1,22 @@
-import { Document } from 'mongoose';
+import { Document } from 'mongoose'
 
 export type BookingInterface = {
-  user_id: string;
-  trip_id: string;
-  booking_seat: string;
-  status: 'pending' | 'approved' | 'completed' | 'cancelled';
-  payment_id?: string;
+  user_id: string
+  trip_id: string
+  booking_seat: string
+  status: 'pending' | 'approved' | 'completed' | 'cancelled'
+  payment_id?: string
 } & Document
 
 export type BookingCreateDTO = {
-  user_id: string;
-  trip_id: string;
-  booking_seat: string[];
+  user_id: {
+    name: string
+    email: string
+  }
+  trip_id: string
+  booking_seat: string[]
 }
 
 export type BookingUpdateDTO = {
-  status: string;
+  status: string
 }
