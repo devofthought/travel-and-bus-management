98d5a9182b05df8f1046e5fb8e99b36bf7acd2cc
ankan112 <ankanhalder112@gmail.com>
2023-12-02 03:13:52 +0600

    404 page update

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,9 +1,26 @@
+import Link from "next/link";
 import React from "react";
+import { BsFillQuestionCircleFill } from "react-icons/bs";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex justify-center items-center h-[100vh]">
-      <h2 className="text-3xl font-bold text-red-500">404 (Not Found)</h2>
+    <div className="flex justify-center text-center items-center h-[100vh]">
+      <div>
+        <BsFillQuestionCircleFill color="#d84e55" size={120} />
+        <h2 className="text-4xl font-semibold mt-3">Page Not Found</h2>
+        <p className="my-2 text-lg font-medium">
+          Oops! We couldn't find the page that you're looking for. <br /> Please
+          check the address and try again.
+        </p>
+        <p className="primary-text">
+          <span className="font-semibold">Error Code:</span> 404
+        </p>
+        <p className="mt-1">
+          <Link href="/" className="underline">
+            Return Home
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
