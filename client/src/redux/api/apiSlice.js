09f01bd1c3f245c219311a58d0f772e799d65077
Md Kamruzzaman <kamruzzaman-dev@users.noreply.github.com>
2023-12-06 20:04:49 +0600

    env

--- client/src/redux/api/apiSlice.js
+++ client/src/redux/api/apiSlice.js
@@ -1,10 +1,11 @@
+import envConfig from "@/config/envConfig";
 import { getFromLocalStorage } from "@/utils/localStorage";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: `${process.env.NEXT_PUBLIC_BASE_URL}api/v1`,
     prepareHeaders: (headers) => {
       headers.set("authorization", getFromLocalStorage("accessToken"));
       return headers;
